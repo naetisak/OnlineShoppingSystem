46476e08597ef151637d59b05481fcce2754859a
unknown <65301282014@utc.ac.th>
2024-01-05 16:31:57 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AdminController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,3 +32,20 @@ Route::middleware([
 
 route::get('/redirect',[HomeController::class,'redirect']);
 
+route::get('/view_catagory',[AdminController::class,'view_catagory']);
+
+route::post('/add_catagory',[AdminController::class,'add_catagory']);
+
+route::get('/delete_catagory/{id}',[AdminController::class,'delete_catagory']);
+
+route::get('/view_product',[AdminController::class,'view_product']);
+
+route::post('/add_product',[AdminController::class,'add_product']);
+
+route::get('/show_product',[AdminController::class,'show_product']);
+
+route::get('/delete_product/{id}',[AdminController::class,'delete_product']);
+
+route::get('/update_product/{id}',[AdminController::class,'update_product']);
+
+route::post('/update_product_confirm/{id}',[AdminController::class,'update_product_confirm']);
